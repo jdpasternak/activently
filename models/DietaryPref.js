5c71ae0041a445b0783d2bd0d2d2c3e38fb3fd9e
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-12 11:33:24 -1000

    Add DietaryPref model

---
+++ models/DietaryPref.js
@@ -0,0 +1,31 @@
+const { Model, DataTypes } = require("sequelize");
+const sequelize = require("../config/connection");
+
+class DietaryPref extends Model {}
+
+DietaryPref.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+      allowNull: false,
+    },
+    name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    description: {
+      type: DataTypes.STRING,
+      allowNull: true,
+    },
+  },
+  {
+    sequelize,
+    freezeTableName: true,
+    underscored: true,
+    modelName: "dietary_pref",
+  }
+);
+
+module.exports = DietaryPref;
