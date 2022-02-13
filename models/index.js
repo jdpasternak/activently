617c14b4f4b13412591a11d1cb24e099409144d7
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-13 11:46:08 -1000

    Remove unneccessary comments from models/index.js

--- models/index.js
+++ models/index.js
@@ -142,65 +142,6 @@ Activity.belongsTo(Interest, {
   foreignKey: "interest_id",
 });
 
-// Interest < UserInterest
-// Interest < Activity
-
-// Activity < Attendance
-// Activity < Comment
-
-// Activity.organizer_id > User.id
-// User.hasMany(Activity, {
-//   foreignKey: "organizer_id",
-// });
-
-// Activity.belongsTo(User, {
-//   foreignKey: "organizer_id",
-// });
-
-// Comment.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
-
-// Comment.belongsTo(Activity, {
-//   foreignKey: "activity_id",
-// });
-
-// User.hasMany(Comment, {
-//   foreignKey: "user_id",
-// });
-
-// Activity.hasMany(Comment, {
-//   foreignKey: "activity_id",
-// });
-
-// User.belongsToMany(Activity, {
-//   through: Attendance,
-//   as: "Attendance_activities",
-//   foreignKey: "user_id",
-// });
-
-// Activity.belongsToMany(User, {
-//   through: Attendance,
-//   as: "Attendance_activities",
-//   foreignKey: "activity_id",
-// });
-
-// Attendance.belongsTo(User, {
-//   foreignKey: "user_id",
-// });
-
-// Attendance.belongsTo(Activity, {
-//   foreignKey: "activity_id",
-// });
-
-// User.hasMany(Attendance, {
-//   foreignKey: "user_id",
-// });
-
-// Activity.hasMany(Attendance, {
-//   foreignKey: "activity_id",
-// });
-
 module.exports = {
   User,
   Activity,
