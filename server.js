773af5e5166636f645d8ea9f4f7c90ff0059a94a
BTV <horsfalm@gmail.com>
2022-02-15 09:16:29 +1100

    add sess dependency and update routes to invoke

--- server.js
+++ server.js
@@ -5,9 +5,25 @@ const sequelize = require("./config/connection");
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
+
+const sess = {
+  secret: process.env.DB_SECRET,
+  cookie: {},
+  resave: false,
+  saveUnitialized: true,
+  store: new SequelizeStore({
+    db: sequelize,
+    checkExpirationInterval: 1000 * 60 * 10, // will check every 10 min
+    expiration: 1000 * 60 * 30 // will expire after 30 minutes
+  })
+};
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(controllers);
+
+app.use(session(sess));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log("Server now listening on port 3001"));
