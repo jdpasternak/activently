e65670187e25a9fe9470f4ef795a28306b5ee59b
Afaed <justchoy99@gmail.com>
2022-02-15 12:16:07 -0800

    added the routes for homepage and what not.

--- controllers/homepage-routes.js
+++ controllers/homepage-routes.js
@@ -1,6 +1,8 @@
 const router = require("express").Router();
+const { where } = require("sequelize/types");
 const sequelize = require("../../config/connection");
-const { user, activity, comment, diet}
+const { User, Activity, Interest, DietaryPref } = require('../models')
+const withAuth = require('../utils/auth')
 //need routes to navigate throughout the app
 //just get routes for events
 //wait a minute Ive alread done this
@@ -17,4 +19,134 @@ router.get("/", (req, res) => {
 //activity/ 
 //id
 
-router.get("/", (req, res))
+router.get('/homepage', (req, res) => {
+  if (req.session.loggedIn) {
+    res.render('homepage');
+    return;
+  } 
+})
+router.get('/login', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('login');
+});
+
+router.get('/signup', (req, res) => {
+  if (req.session.loggedIn) {
+    res.redirect('/');
+    return;
+  }
+
+  res.render('signup');
+});
+
+router.get('/profile', withAuth, (req, res) => {
+  if (req.session.loggedIn) {router.get(User, Activity, Interest), (req, res) => {
+    User.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: {
+        user_id,
+        name,
+      }
+    })
+  }}
+})
+router.get("/", withAuth, (req, res) => {
+  
+})
+router.get("/", withAuth, (req, res) => {
+  Interest.findOne({
+    where: {
+      id: req.params.id
+    },
+    attributes: [
+      'id',
+      'name',
+      'description'
+    ],
+    include: [
+      {
+      model: Interest,
+      attributes: [
+        'id',
+        'name',
+        'description'
+      ]
+      }
+    ]
+  })
+})
+router.get("/", withAuth, (req, res) => {
+  Activity.findAll ({
+  attribures: [
+    'id',
+    'title',
+    'description',
+    'location',
+    'occurence',
+    'organizer_id',
+    'is_private',
+    'seats',
+    'rules',
+    'price',
+    'req_dietary_pref',
+    'interest_id'
+  ],
+  include: [{
+    model: 'Activity',
+    attributes: ['id',
+    'title',
+    'description',
+    'location',
+    'occurence',
+    'organizer_id',
+    'is_private',
+    'seats',
+    'rules',
+    'price',
+    'req_dietary_pref',
+    'interest_id']
+  }
+  ]
+    })
+    .then(dbactivityData => {
+      const activity =dbactivityData.map(activity => activity.get({ plain: true }));
+      res.render('activity') ({ posts, loggedIn: true });
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err)
+    })
+});
+/* organizer_id: {
+      type: DataTypes.INTEGER,
+      references: {
+        model: "user",
+        key: "id",
+      },
+    },
+    is_private: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+    seats: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    rules: {
+      type: DataTypes.STRING,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+    },
+    req_dietary_pref: {
+      type: DataTypes.BOOLEAN,
+    },
+    interest_id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,*/
