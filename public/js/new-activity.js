e3ef8251ac607a8ca3dfc43940120da5766d3aa9
naomi <naomilounsbury@hotmail.com>
2022-02-14 18:48:05 -0800

    fixed new activity.js a bit, updated updateactivity.js

--- public/js/new-activity.js
+++ public/js/new-activity.js
@@ -1,16 +1,16 @@
 async function newActivity(event) {
     event.preventDefault();
   
-    const title = document.querySelector('input[id="activity_title"]').value;
-    const description = document.querySelector('input[id="description"]').value;
-    const location = document.querySelector('input[id="zip"]').value;
-    const occurrence = document.querySelector('input[id="date"]').value;
+    const title = document.querySelector('input[name="activity_title"]').value;
+    const description = document.querySelector('input[name="description"]').value;
+    const location = document.querySelector('input[name="zip"]').value;
+    const occurrence = document.querySelector('input[name="date"]').value;
     // need to grab the id and I will look it up tomorrow
-    const organizer_id = document.querySelector('input[id=""]').value;
+    const organizer_id = document.querySelector('label[class="organizer-id"]').value;
     //how do I do a boolean
-    const is_private = document.querySelector('checkbox[id=""]').value;
+    const is_private = document.querySelector('checkbox[name="is-private"]').value;
 
-    const seats = document.querySelector('input[id="seats"]')
+    const seats = document.querySelector('input[name="seats"]')
 
     const response = await fetch(`/api/activities`, {
       method: 'POST',
--- public/js/update-activity.js
+++ public/js/update-activity.js
@@ -1,7 +1,26 @@
+const { Activity } = require("../../models");
+
 async function updateActivities(event) {
     event.preventDefault();
-  
+    const formData = new FormData();
     const title = document.querySelector('input[name="activity-title"]').value.trim();
+    const description = document.querySelector('input[name="activity-title"]').value.trim();
+    const location = document.querySelector('input[name="activity-title"]').value.trim();
+    const occurrence = document.querySelector('input[name="activity-title"]').value.trim();
+    const organizer_id = document.querySelector('input[name="activity-title"]').value.trim();
+    const is_private = document.querySelector('input[name="activity-title"]').value.trim();
+    const seats = document.querySelector('input[name="activity-title"]').value.trim();
+
+// formData.append('title', `${title}`);
+// formData.append('description', `${description}`);
+// formData.append('location', `${location}`);
+// formData.append('occurrence', `${}`);
+// formData.append('organizer_id',`${}` );
+// formData.append('is_private', `${}`);
+// formData.append('seats', );
+  
+    
+
     //so i dont really understand this code but it seems to work in the 
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
