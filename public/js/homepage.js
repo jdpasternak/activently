2340073562c967f3d7d42ba25fc0fab12112fcae
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-15 15:15:01 -1000

    Add TODOs and FIXMEs to /public/js/homepage.js

--- public/js/homepage.js
+++ public/js/homepage.js
@@ -1,21 +1,19 @@
-const zipCodeInputEl =document.querySelector(input).value
-function getLocation(){
-    zipCodeInputEl.innerHTML = "";
-    const requestUrl = `https://service.zipapi.us/zipcode/${zipCodeInputEl}/?X-API-KEY=978c0688a92271d0820408efd9d332be`
-    fetch(requestUrl)
-        .then((response)=>{
-            return response.json
+// TODO add some comments to describe what's going on here
+const zipCodeInputEl = document.querySelector(input).value;
 
-        })
-        .then((zipcodeData)=>{
-            getCityFromZip(zipcodeData)
-
-        }) 
+function getLocation() {
+  zipCodeInputEl.innerHTML = "";
+  const requestUrl = `https://service.zipapi.us/zipcode/${zipCodeInputEl}/?X-API-KEY=978c0688a92271d0820408efd9d332be`;
+  fetch(requestUrl)
+    .then((response) => response.json)
+    .then((zipcodeData) => getCityFromZip(zipcodeData));
 }
-function getCityFromZip(zipCodeData){
-    const cityEl = document.querySelector("#city-name");
-    cityEl.innerHTML = `${data.name}`
 
+// FIXME zipCodeData is not used within the function
+function getCityFromZip(zipCodeData) {
+  const cityEl = document.querySelector("#city-name");
+  cityEl.innerHTML = `${data.name}`;
 }
-const zipCodeSearch = document.querySelector(#zipCodeBtn)
+
+const zipCodeSearch = document.querySelector("#zipCodeBtn");
 zipCodeSearch.addEventListener("submit", getLocation);
