2fb1c913e7c7654bcd8685faf400b5f28554350e
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-15 15:27:02 -1000

    prettier

--- public/js/signup.js
+++ public/js/signup.js
@@ -1,31 +1,32 @@
-
 async function signupFormHandler(event) {
-    event.preventDefault();
-  
-    const username = document.querySelector('#username-signup').value.trim();
-    const email = document.querySelector('#email-signup').value.trim();
-    const password = document.querySelector('#password-signup').value.trim();
-    const userDietaryPrefs = document.querySelector("#UserDietaryPrefs");
-    const interests = document.querySelector()
-  
-    if (username && email && password && userDietaryPrefs && interests) {
-      const response = await fetch('/api/users', {
-        method: 'post',
-        body: JSON.stringify({
-          username,
-          email,
-          pass,
-          userDietaryPrefs,
-          interests
-        }),
-        headers: { 'Content-Type': 'application/json' }
-      });
-  
-      if (response.ok) {
-        document.location.replace('/browsing/');
-      } else {
-        alert(response.statusText);
-      }
+  event.preventDefault();
+
+  const username = document.querySelector("#username-signup").value.trim();
+  const email = document.querySelector("#email-signup").value.trim();
+  const password = document.querySelector("#password-signup").value.trim();
+  const userDietaryPrefs = document.querySelector("#UserDietaryPrefs");
+  const interests = document.querySelector();
+
+  if (username && email && password && userDietaryPrefs && interests) {
+    const response = await fetch("/api/users", {
+      method: "post",
+      body: JSON.stringify({
+        username,
+        email,
+        pass,
+        userDietaryPrefs,
+        interests,
+      }),
+      headers: { "Content-Type": "application/json" },
+    });
+
+    if (response.ok) {
+      document.location.replace("/browsing/");
+    } else {
+      alert(response.statusText);
     }
   }
-  document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
+}
+document
+  .querySelector(".signup-form")
+  .addEventListener("submit", signupFormHandler);
