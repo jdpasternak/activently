0354415180714ffe098ac4b5e62e8beea948d79a
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-15 15:24:00 -1000

    prettier

--- public/js/new-activity.js
+++ public/js/new-activity.js
@@ -1,40 +1,43 @@
 async function newActivity(event) {
-    event.preventDefault();
-  
-    const title = document.querySelector('input[name="activity_title"]').value;
-    const description = document.querySelector('input[name="description"]').value;
-    const location = document.querySelector('input[name="zip"]').value;
-    const occurrence = document.querySelector('input[name="date"]').value;
-    // need to grab the id and I will look it up tomorrow
-    const organizer_id = document.querySelector('label[class="organizer-id"]').value;
-    //how do I do a boolean
-    const is_private = document.querySelector('checkbox[name="is-private"]').value;
+  event.preventDefault();
 
-    const seats = document.querySelector('input[name="seats"]')
+  const title = document.querySelector('input[name="activity_title"]').value;
+  const description = document.querySelector('input[name="description"]').value;
+  const location = document.querySelector('input[name="zip"]').value;
+  const occurrence = document.querySelector('input[name="date"]').value;
+  // need to grab the id and I will look it up tomorrow
+  const organizer_id = document.querySelector(
+    'label[class="organizer-id"]'
+  ).value;
+  //how do I do a boolean
+  const is_private = document.querySelector(
+    'checkbox[name="is-private"]'
+  ).value;
 
-    const response = await fetch(`/api/activities`, {
-      method: 'POST',
-      body: JSON.stringify({
-        title,
-        description,
-        location,
-        occurrence, 
-        organizer_id,
-        is_private,
-        seats,
-        interest_id,
-       
-      }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    });
-  
-    if (response.ok) {
-      document.location.replace('/browsing');
-    } else {
-      alert(response.statusText);
-    }
+  const seats = document.querySelector('input[name="seats"]');
+
+  const response = await fetch(`/api/activities`, {
+    method: "POST",
+    body: JSON.stringify({
+      title,
+      description,
+      location,
+      occurrence,
+      organizer_id,
+      is_private,
+      seats,
+      interest_id,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  if (response.ok) {
+    document.location.replace("/browsing");
+  } else {
+    alert(response.statusText);
   }
-  
-  document.querySelector('.new-Activity').addEventListener('submit', newActivity);
+}
+
+document.querySelector(".new-Activity").addEventListener("submit", newActivity);
