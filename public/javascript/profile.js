c47f811d036e1cefb247f36f6a48e1c8b1614cd3
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-16 13:50:14 -1000

    add input fields to basic info modal

---
+++ public/javascript/profile.js
@@ -0,0 +1,10 @@
+const $editBasicInfoButton = document.querySelector("#edit-basic-info-button");
+
+// const editBasicInfoButtonHandler = (event) => {
+//   event.preventDefault();
+// };
+
+document.addEventListener("DOMContentLoaded", () => {
+  const elems = document.querySelectorAll(".modal");
+  const instances = M.Modal.init(elems);
+});
