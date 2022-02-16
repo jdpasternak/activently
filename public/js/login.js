8515f2cb1b46a4c37549e6431256da5ed4bc204d
naomi <naomilounsbury@hotmail.com>
2022-02-16 13:42:38 -0800

    small changes to login

--- public/js/login.js
+++ public/js/login.js
@@ -1,5 +1,5 @@
 // [ ] TODO add some comments to describe what's going on here
-
+//this is to login 
 async function loginFormHandler(event) {
   event.preventDefault();
 
@@ -22,6 +22,31 @@ async function loginFormHandler(event) {
       // [ ] TODO make this a modal instead of a standard browser alert
       alert(response.statusText);
     }
+  }
+}
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
   }
 }
 
