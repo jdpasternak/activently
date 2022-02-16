55009bf0ddaf51f9ddce1fa9f3964d546659e4f0
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-15 15:22:07 -1000

    remove trim() on password as they may contain spaces

--- public/js/login.js
+++ public/js/login.js
@@ -2,7 +2,7 @@ async function loginFormHandler(event) {
   event.preventDefault();
 
   const email = document.querySelector("#email-login").value.trim();
-  const password = document.querySelector("#password-login").value.trim();
+  const password = document.querySelector("#password-login").value;
 
   if (email && password) {
     const response = await fetch("/api/users/login", {
