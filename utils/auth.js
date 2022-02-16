87cc101daeeed7510690d8aad56b5d062a353b00
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-15 14:37:43 -1000

    fix module exports

--- utils/auth.js
+++ utils/auth.js
@@ -1,39 +1,22 @@
 const withAuth = (req, res, next) => {
-<<<<<<< HEAD
-    if (!req.session.user_id) {
-      res.redirect('/login');
-    } else {
-      next();
-    }
-  };
-  
-  const withOwned = (req, res, next) => {
-      if (!req.user_id === user_id){
-          res.delete()
-          res.put()
-      }
-      }
-  module.exports = withAuth;
-=======
- if (!req.session.user.id) {
-     res.redirect('/login');
- }   else {
-     next()
- }
-}
-module.exports = withAuth;
-
-const withOwned =  (req, res, next) => {
-  if (withAuth)
-  if (!req.session.user_id, req.session.activity_id === activity.organizer_id) {
-        res.redirect('/homepage');
-    }   else {
-        next()
-    }
+  if (!req.session.user.id) {
+    res.redirect("/login");
+  } else {
+    next();
   }
+};
 
-   module.exports= withOwned
+const withOwned = (req, res, next) => {
+  if (
+    (!req.session.user_id, req.session.activity_id === activity.organizer_id)
+  ) {
+    res.redirect("/homepage");
+  } else {
+    next();
+  }
+};
 
-   //we may need the organizer id and sql to get data from the activity
-   //Key question: 
->>>>>>> helpers
+module.exports = { withAuth, withOwned };
+
+//we may need the organizer id and sql to get data from the activity
+//Key question:
