941917669fa2a170c0ce1c1ec0bdc3314e469030
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-16 16:32:54 -1000

    enables modifying dietary preferences

--- public/javascript/profile.js
+++ public/javascript/profile.js
@@ -2,13 +2,22 @@ const $editBasicInfoButton = document.querySelector("#edit-basic-info-button");
 const $saveEditBasicInfoButton = document.querySelector(
   "#save-edit-basic-info-button"
 );
+
+const $editDietaryPreferencesButton = document.querySelector(
+  "#edit-dietary-preferences-button"
+);
+const $saveEditDietaryPreferencesButton = document.querySelector(
+  "#save-edit-dietary-prefs-button"
+);
+
 const $usernameInput = document.querySelector("#edit-username");
 const $zipCodeInput = document.querySelector("#edit-zip");
 
-// const editBasicInfoButtonHandler = (event) => {
-//   event.preventDefault();
-// };
+let $dietaryPrefSelectInstance;
 
+/* 
+    Handle button on edit basic info modal to save info
+*/
 const saveEditBasicInfoHandler = async (event) => {
   event.preventDefault();
 
@@ -33,9 +42,41 @@ const saveEditBasicInfoHandler = async (event) => {
   }
 };
 
+const saveEditDietaryPreferencesHandler = (event) => {
+  event.preventDefault();
+
+  const values = $dietaryPrefSelectInstance.getSelectedValues();
+  console.log(values);
+
+  fetch("/api/users/dietaryPrefs", {
+    method: "DELETE",
+  })
+    .then(() => {
+      values.forEach((value) => {
+        fetch("/api/userDietaryPrefs", {
+          method: "POST",
+          body: JSON.stringify({
+            user_id:
+              document.querySelector("#user-id").attributes["data-user-id"]
+                .value,
+            dietary_pref_id: value,
+          }),
+          headers: { "Content-Type": "application/json" },
+        });
+      });
+    })
+    .then(() => {
+      location.replace("/profile");
+    });
+};
+
 document.addEventListener("DOMContentLoaded", () => {
-  const elems = document.querySelectorAll(".modal");
-  const options = {
+  const $editBasicInfoModal = document.querySelector("#edit-basic-info-modal");
+  const $editDietaryPreferencesModal = document.querySelector(
+    "#edit-dietary-preferences-modal"
+  );
+
+  M.Modal.init($editBasicInfoModal, {
     onOpenStart: () => {
       const userUsername = document
         .querySelector("#user-username")
@@ -52,8 +93,30 @@ document.addEventListener("DOMContentLoaded", () => {
       $usernameInput.value = userUsername;
       $zipCodeInput.value = userZipCode;
     },
-  };
-  const instances = M.Modal.init(elems, options);
+  });
+
+  const $dietaryPrefSelect = document.querySelector("#dietary-pref-select");
+
+  M.Modal.init($editDietaryPreferencesModal, {
+    onOpenStart: async () => {
+      const response = await fetch("/api/dietaryPrefs")
+        .then((apiInterestData) => apiInterestData.json())
+        .then((data) => {
+          data.forEach((i) => {
+            console.log(i);
+            let $option = document.createElement("option");
+            $option.value = i.id;
+            $option.textContent = i.name;
+            $dietaryPrefSelect.appendChild($option);
+          });
+        });
+      $dietaryPrefSelectInstance = M.FormSelect.init($dietaryPrefSelect);
+    },
+  });
 
   $saveEditBasicInfoButton.addEventListener("click", saveEditBasicInfoHandler);
+  $saveEditDietaryPreferencesButton.addEventListener(
+    "click",
+    saveEditDietaryPreferencesHandler
+  );
 });
