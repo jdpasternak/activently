7bc8dc03c6259def919d738b1444d9b35eb01dcf
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-16 14:39:57 -1000

    add DietaryPref routes file and GET route

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -6,6 +6,7 @@ const commentRoutes = require("./comment-routes");
 const userDietaryPrefRoutes = require("./user-dietary-pref-routes");
 const userInterestRoutes = require("./user-interest-routes");
 const interestRoutes = require("./interest-routes");
+const dietaryPrefRoutes = require("./dietaryPrefs-routes");
 
 router.use("/users", userRoutes);
 router.use("/activities", activityRoutes);
@@ -13,5 +14,6 @@ router.use("/comments", commentRoutes);
 router.use("/userDietaryPrefs", userDietaryPrefRoutes);
 router.use("/userInterests", userInterestRoutes);
 router.use("/interests", interestRoutes);
+router.use("/dietaryPrefs", dietaryPrefRoutes);
 
 module.exports = router;
