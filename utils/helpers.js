99e87d04a8ab0429d882f387ce0961ccb37fde40
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-17 12:31:02 -1000

    add is_organizer helper function to check if the view of the page is the organizer of the activity

--- utils/helpers.js
+++ utils/helpers.js
@@ -1,17 +1,18 @@
-
-
 //function to procces the date and the plurals.
 module.exports = {
-    format_date: date => {
-      return `${new Date(date).getMonth() + 1}/${new Date(date).getDate()}/${new Date(
-        date
-      ).getFullYear()}`;
-    },
-    format_plural: (word, amount) => {
-        if (amount !== 1) {
-          return `${word}s`;
-        }
-    
-        return word;
-    },
-  }
+  format_date: (date) => {
+    return `${new Date(date).getMonth() + 1}/${new Date(
+      date
+    ).getDate()}/${new Date(date).getFullYear()}`;
+  },
+  format_plural: (word, amount) => {
+    if (amount !== 1) {
+      return `${word}s`;
+    }
+
+    return word;
+  },
+  is_owner: (user_id, activity_id) => {
+    return user_id === activity_id;
+  },
+};
