51da75b9fb018770139d0c90a1380643f0e8bc4b
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-16 17:22:16 -1000

    add COMMENTs and FIXME to /public/js/landing.js

--- public/js/landing.js
+++ public/js/landing.js
@@ -1,19 +1,21 @@
 // [ ] TODO add comments to describe what's going on here
 //
 async function searchZipEvents() {
-  // FIXME getZip is not used
+  // [x] FIXME getZip is not used
   const getZip = document.querySelector("#zipCode").value;
 
-  // FIXME ...zip=? `?` must be a template string input -> ${x}
+  // [x] FIXME ...zip=? `?` must be a template string input -> ${x}
   const response = await fetch(`/activities/${getZip}`, {
     method: "GET",
   });
   console.log(response);
   // FIXME Current Behavior: if we get a valid response, we'll reload the page.  Expected Behavior: if we get a valid response, render a page that displays all upcoming events in the user's zip code
+  // COMMENT we don't current have a route for /activity/<zip-code>. Should this be a different route?
   if (response.ok) {
     document.location.replace(`/activity/${getZip}`);
   }
 }
 
 // [ ] TODO  "button" could be ambiguous. Best practice: use a unique ID unless applying action to multiple elements (class)
+// FIXME "getZipBtn" is being used as a tag selector, but here is no such tag.
 document.querySelector("getZipBtn").addEventListener("submit", searchZipEvents);
