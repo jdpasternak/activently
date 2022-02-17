106b96fafd44f752a470a58de4ecf067246a7910
naomi <naomilounsbury@hotmail.com>
2022-02-16 22:50:34 -0800

    updates the user

--- public/js/update-profile.js
+++ public/js/update-profile.js
@@ -1,58 +1,59 @@
 // [x] TODO add comments to describe what's happening in this script
-
-const { getRounds } = require("bcrypt");
-
-/** This function updates the username and the password by grabbing it from the edit-profilehandlebars and does minimal validation on the username and password to check they arent empty */
-async function updateProfiles(event) {
-  event.preventDefault();
-
-  // FIXME title is declared but never used
-  const username = document
-    .querySelector('input[id="username-edit"]')
-    .value.trim();
-
-  const password = document
-    .querySelector('input[id="password-edit"]')
-    .value.trim();
-
-  // FIXME for this, we can grab the profile (or user) ID from req.session.user_id
-  const id = await fetch(`/api/users/${req.session.user_id}`, {
-    method: "GET",
-  });
-
-  // COMMENT should we allow the user to change their password? Here, only `username` can be changed
-  //I think we can
-  const response = await fetch(`/api/users/${req.session.user_id}`, {
-    method: "PUT",
-    body: JSON.stringify(getDataToUpdate()),
-    headers: {
-      "Content-Type": "application/json",
-    },
-  });
-
-  // FIXME the user should be routed back to their pofile view (not edit-profile view) instead of the /browsing view
-  if (response.ok) {
-    document.location.replace("/userprofile");
-  } else {
-    // [ ] TODO use a modal instead of browser alert
-    instances.open("That doesn't look like the right data");
-  }
-}
-
-function getDataToUpdate() {
-  const data = {};
-  if (password && password.length > 0) {
-    data.password = bcrypt(password);
-  }
-  if (username && username.length > 0) {
-    data.username = username;
-  }
-  return data;
-}
 document.addEventListener("DOMContentLoaded", function () {
   var elems = document.querySelectorAll(".modal");
-  var instances = M.Modal.init(elems, options);
+  //var instances = M.Modal.init(elems, options);
+
+  /** This function updates the username and the password by grabbing it from the edit-profilehandlebars and does minimal validation on the username and password to check they arent empty */
+  async function updateProfiles(event) {
+    event.preventDefault();
+
+    // FIXME title is declared but never used
+    const username = document
+      .querySelector('input[id="username-edit"]')
+      .value.trim();
+
+    const password = document
+      .querySelector('input[id="password-edit"]')
+      .value.trim();
+
+    // FIXME for this, we can grab the profile (or user) ID from req.session.user_id
+    // no we cannot because req.session.user_id only works from the backend
+    const urlArr = window.location.pathname.split("/");
+    const id = urlArr[urlArr.length - 1];
+
+    // COMMENT should we allow the user to change their password? Here, only `username` can be changed
+
+    const response = await fetch(`/api/users/${id}`, {
+      method: "PUT",
+      body: JSON.stringify({
+        username,
+        password,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    // FIXME the user should be routed back to their pofile view (not edit-profile view) instead of the /browsing view
+    if (response.ok) {
+      document.location.replace(`/profile/${id}`);
+    } else {
+      // [ ] TODO use a modal instead of browser alert
+      //instances.open();
+    }
+  }
+
+  //   function getDataToUpdate() {
+  //     const data = {};
+  //     if (password && password.length > 0) {
+  //       data.password = password;
+  //     }
+  //     if (username && username.length > 0) {
+  //       data.username = username;
+  //     }
+  //     return data;
+  //   }
+  document
+    .querySelector(".edit-profile-form")
+    .addEventListener("submit", updateProfiles);
 });
-document
-  .querySelector(".edit-profile-form")
-  .addEventListener("submit", updateProfiles);
