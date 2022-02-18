c42916eaa8902e4a4397e68d73db58ceeb3a8022
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-18 13:13:26 -1000

    adds post and delete routes to invitation-routes

--- controllers/api/invitation-routes.js
+++ controllers/api/invitation-routes.js
@@ -10,6 +10,34 @@ router.get("/", (req, res) => {
     include: [
       {
         model: User,
+        attributes: ["id", "username"],
+        foreignKey: "user_id",
+      },
+      {
+        model: Activity,
+        attributes: ["id", "title"],
+        foreignKey: "activity_id",
+      },
+    ],
+  })
+    .then((dbInvitationData) => {
+      res.json(dbInvitationData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/* 
+    GET /api/invitations/:id
+*/
+router.get("/:id", (req, res) => {
+  Invitation.findOne({
+    where: { id: req.params.id },
+    include: [
+      {
+        model: User,
         foreignKey: "user_id",
       },
       {
@@ -19,6 +47,44 @@ router.get("/", (req, res) => {
     ],
   })
     .then((dbInvitationData) => {
+      if (!dbInvitationData) {
+        res.status(404).json({ message: "No invitation found with that ID" });
+      }
+      res.json(dbInvitationData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/* 
+    POST /api/invitations
+    CREATEs an invitation record
+*/
+router.post("/", (req, res) => {
+  Invitation.create({
+    user_id: req.session.user_id || req.body.user_id,
+    activity_id: req.body.activity_id,
+  })
+    .then((dbInvitationData) => {
+      res.json(dbInvitationData);
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+/* 
+    DELETE /api/invitations/:id
+*/
+router.delete("/:id", (req, res) => {
+  Invitation.destroy({ where: { id: req.params.id } })
+    .then((dbInvitationData) => {
+      if (!dbInvitationData) {
+        res.status(404).json({ message: "No invitation with that ID" });
+      }
       res.json(dbInvitationData);
     })
     .catch((err) => {
