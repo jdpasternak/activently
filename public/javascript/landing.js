cc2ab91ff2892994ddb0eae27cea8a2a9cb4576f
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-18 09:45:19 -1000

    enables searching for activities by zip code

--- public/javascript/landing.js
+++ public/javascript/landing.js
@@ -1,15 +1,14 @@
 // [ ] TODO add comments to describe what's going on here
 //this function is seaching the events and getting the event by zip code
+const re = /^\d{5}(?:[- ]?\d{4})?$/;
+
 async function searchZipEvents() {
-  const getZip = document.querySelector("#zipCode").value;
-
-  const response = await fetch(`/${getZip}`, {
-    method: "GET",
-  });
-  console.log(response);
-
-  if (response.ok) {
-    document.location.replace(`/${getZip}`);
+  const zipCode = document.querySelector("#zipCode").value;
+  const zipCodeMatch = zipCode.match(re);
+  if (zipCodeMatch) {
+    location.replace(`/activity/zip/${zipCode}`);
+  } else {
+    // alert or set warning on the field
   }
 }
 document.querySelector(".zipSearch").addEventListener("click", searchZipEvents);
