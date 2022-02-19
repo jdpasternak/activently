e2d10cd93a7842a402e29e0b92bd5410a42ec08a
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-18 14:31:05 -1000

    enables sending invitations to user by adding invitation to database

--- public/javascript/invite.js
+++ public/javascript/invite.js
@@ -4,7 +4,6 @@ let inviteSelectInst;
 
 document.addEventListener("DOMContentLoaded", () => {
   const $inviteModal = document.querySelectorAll("#inviteModal");
-  console.log($inviteModal);
   const inviteModalInst = M.Modal.init($inviteModal, {
     preventScrolling: false,
   });
@@ -12,6 +11,10 @@ document.addEventListener("DOMContentLoaded", () => {
   populateUsers().then(() => {
     inviteSelectInst = M.FormSelect.init($inviteSelect);
   });
+
+  document
+    .querySelector("#sendInvitationsButton")
+    .addEventListener("click", sendInvitationsButtonHandler);
 });
 
 const populateUsers = async () => {
@@ -25,4 +28,35 @@ const populateUsers = async () => {
         $inviteSelect.appendChild($option);
       });
     });
+};
+
+const sendInvitationsButtonHandler = (event) => {
+  const values = inviteSelectInst.getSelectedValues();
+
+  if (values[0] === "") {
+    console.log("empty");
+    return;
+  }
+
+  const activity_id = location.toString().split("/")[
+    location.toString().split("/").length - 1
+  ];
+
+  values.forEach((value) => {
+    fetch("/api/invitations", {
+      method: "POST",
+      body: JSON.stringify({
+        user_id: value,
+        activity_id: activity_id,
+      }),
+      headers: { "Content-Type": "application/json" },
+    })
+      .then((response) => response.json())
+      .then((apiInvitationData) => console.log(apiInvitationData))
+      .catch((err) => {
+        console.log(err);
+      });
+  });
+
+  console.log(values);
 };
