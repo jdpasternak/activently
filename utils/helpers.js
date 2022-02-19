c8ecae100064d28c4dd4baf24168e48eb3021cc4
Jake Pasterank <jdp.pasternak@gmail.com>
2022-02-18 16:03:17 -1000

    updates is_invited and is_attending helper functions

--- utils/helpers.js
+++ utils/helpers.js
@@ -16,7 +16,13 @@ module.exports = {
   is_organizer: (user_id, activity_id) => {
     return user_id === activity_id;
   },
-  is_invited: (user_id, attending) => {
+  is_invited: (user_id, invited) => {
+    return invited.find((i) => i.id === user_id);
+  },
+  is_attending: (user_id, attending) => {
+    if (!attending) {
+      return false;
+    }
     return attending.find((i) => i.id === user_id);
   },
 };
